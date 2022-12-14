db781e7b09643642b6f6e8b846f48c9520e9b576
Matias Muzzo <matiasbiasioli@gmail.com>
2022-12-14 13:39:34 -0300

    ultimos cambios miercoles

--- js/main.js
+++ js/main.js
@@ -35,59 +35,84 @@ alert(
 
 //Constantes lista de entradas
 
-const listaDeEntradas = [
-  { nombre: " 1. Camarones ", precio: "1200" },
-  { nombre: " 2. Arroz con palta y sesamo ", precio: "1400" },
-  { nombre: " 3. Salmon ", precio: "2500" },
-  { nombre: " 4. Empanadas primavera ", precio: "1400" },
-];
-
-let todosLasEntradas = listaDeEntradas.map(
-  (listaDeEntradas) => listaDeEntradas.nombre + listaDeEntradas.precio + "$"
-);
-alert(todosLasEntradas.join(" - "));
-
-let entradas = prompt("Ingrese aquí el numero de entrada seleccionada");
-
-if (entradas == 1) {
-  alert("Bien, agregaste unos Camarones a tu pedido");
-} else if (entradas == 2) {
-  alert("Bien, agregaste un Arroz con palta y sesamos a tu pedido");
-} else if (entradas == 3) {
-  alert("Bien, agregaste un salmon tu pedido");
-} else if (entradas == 4) {
-  alert("Bien, agregaste unas empanadas primavera tu pedido");
+function menuEntradas() {
+  const listaDeEntradas = [
+    { nombre: " 1. Camarones ", precio: "1200" },
+    { nombre: " 2. Arroz con palta y sesamo ", precio: "1400" },
+    { nombre: " 3. Salmon ", precio: "2500" },
+    { nombre: " 4. Empanadas primavera ", precio: "1400" },
+  ];
+  let todosLasEntradas = listaDeEntradas.map(
+    (listaDeEntradas) => listaDeEntradas.nombre + listaDeEntradas.precio + "$"
+  );
+  alert(todosLasEntradas.join(" - "));
 }
+menuEntradas();
+
+function selccionEntrada() {
+  let entradas = prompt("Ingrese aquí el numero de entrada seleccionada");
+  if (entradas == 1) {
+    alert("Bien, agregaste unos Camarones a tu pedido");
+  } else if (entradas == 2) {
+    alert("Bien, agregaste un Arroz con palta y sesamos a tu pedido");
+  } else if (entradas == 3) {
+    alert("Bien, agregaste un salmon tu pedido");
+  } else if (entradas == 4) {
+    alert("Bien, agregaste unas empanadas primavera tu pedido");
+  }
+}
+selccionEntrada();
 
 function cantidades() {
   let cantidad = prompt(
     "Indicanos el numero de entradas que quieres agregar al pedido"
   );
   if (cantidad == 1) {
-    alert ("Perfecto, una sola entrada");
-  } else (entradas >= 2); {
-    alert ("Excelente, serán " + cantidad + " entradas");
+    alert("Perfecto, una sola entrada");
+  } else {
+    alert("Excelente, serán " + cantidad + " entradas seleccionadas");
   }
 }
+cantidades();
+
+function agregarMas() {
+  let agregar = prompt(
+    "¿Queres agregar otra entrada al pedido?, responde con si o no"
+  );
+  while (agregar == "si") {
+    menuEntradas();
+    selccionEntrada();
+    cantidades();
+    agregar = prompt(
+      "¿Queres agregar otra entrada al pedido?, responde con si o no")
+  }
+  if (agregar != "si");
+  {
+  }
+}
+agregarMas();
 
 alert(
-  "En el siguiente cuadro verás una lista de entradas disponibles, responde con el numero de plato seleccionado"
+  "En el siguiente cuadro verás una lista de platos disponibles, responde con el numero de plato seleccionado"
 );
 
-const listaDePlatos = [
-  { nombre: "1. Chao-fan ", precio: "2200" },
-  { nombre: "2. Chao-mien ", precio: "2200" },
-  { nombre: "3. Sushi ", precio: "3000" },
-  { nombre: "4. Sopa de arroz ", precio: "1800" },
-];
+function menuPlatos() {
+  const listaDePlatos = [
+    { nombre: "1. Chao-fan ", precio: "2200" },
+    { nombre: "2. Chao-mien ", precio: "2200" },
+    { nombre: "3. Sushi ", precio: "3000" },
+    { nombre: "4. Sopa de arroz ", precio: "1800" },
+  ];
+  let todosLosPlatos = listaDePlatos.map(
+    (listaDePlatos) => listaDePlatos.nombre + listaDePlatos.precio + "$"
+  );
+  alert(todosLosPlatos.join(" - "));
+}
+menuPlatos()
 
-let todosLosPlatos = listaDePlatos.map(
-  (listaDePlatos) => listaDePlatos.nombre + listaDePlatos.precio + "$"
-);
-alert(todosLosPlatos.join(" - "));
 
-let productos = prompt("Ingrese aquí su número de plato seleccionado");
-
+function selccionEntrada2() {
+  let productos = prompt("Ingrese aquí su número de plato seleccionado");
 if (productos == 1) {
   alert("Bien, agregaste un chao-fan a tu pedido");
 } else if (productos == 2) {
@@ -97,6 +122,38 @@ if (productos == 1) {
 } else if (productos == 4) {
   alert("Bien, agregaste una sopa de arroz a tu pedido");
 }
+}
+
+
+function cantidades2() {
+  let cantidad = prompt(
+    "Indicanos el numero de platos que quieres agregar al pedido"
+  );
+  if (cantidad == 1) {
+    alert("Perfecto, un solo plato");
+  } else
+  {
+    alert("Excelente, serán " + cantidad + " plato");
+  }
+}
+cantidades2();
+
+function agregarMas2() {
+  agregar = prompt(
+    "¿Queres agregar otra entrada al pedido?, responde con si o no"
+  );
+  while (agregar == "si") {
+    menuPlatos();
+    selccionEntrada2();
+    cantidades2();
+    agregar = prompt(
+      "¿Queres agregar otra entrada al pedido?, responde con si o no")
+  }
+  if (agregar != "si");
+  {
+  }
+}
+agregarMas2();
 
 let correo = prompt("Dejanos tu email a continuación");
 
